0933e4ad63747e8033daa29ee122c4906f8d102d
Dylan Smith <Dylan.Smith@shopify.com>
2011-09-02 12:15:23 -0400

    Support App Engine along with regular Django applications.
    * Add python 2.5 compatibility.
    * Update the README.md file to include instructions for deploying on
      Google App Engine.

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 Shopify Django App Example
 ==========================
 
-This package makes it easy to get a Django app up and running with
-the [Python Shopify API](https://github.com/shopify/shopify_python_api).
+This project makes it easy to get a Shopify app up and running with
+[Django](https://www.djangoproject.com/) and the
+[Python Shopify API](https://github.com/shopify/shopify_python_api).
 
-This package contains two django apps, `shopify_app` which contains
-handles authentication and `home` which contains an example of how
-to use the API. Currently `home` displays basic information about
-the shop's products and orders, but is meant to be modified or
-replaced to create your Shopify App.
+This project contains
+[this example Shopify app](http://shopify-django-example.appspot.com),
+which simply displays basic information about the shop's products
+and orders. This project contains two django apps, `shopify_app`
+which handles authentication (meant to be reusable) and `home`
+which contains the example code to demonstrate how to use the API
+(meant to be modified or replaced to create your Shopify App).
 
 Requirements
 ------------
@@ -27,18 +30,65 @@ visiting https://YOUR-SHOP.myshopify.com/admin/api
 Regular Django Application
 --------------------------
 
-1. Obtain your applications API Key and Shared Secret, and modify
-   `shopify_settings.py` to use these values.
+1.  Obtain your applications API Key and Shared Secret, and modify
+    `shopify_settings.py` to use these values.
 
-2. Install the pre-requisites:
+2.  Install the pre-requisites:
 
-   `easy_install Django ShopifyAPI PyYAML pyactiveresource`
+    `easy_install Django ShopifyAPI PyYAML pyactiveresource`
 
-3. Start the server
+3.  Start the server
 
-   `python manage.py runserver`
+    `python manage.py runserver`
 
-4. Visit <http://localhost:8000> to view the example.
+4.  Visit <http://localhost:8000> to view the example.
+
+Google App Engine
+-----------------
+
+1.  [Create an application](https://appengine.google.com/start) with
+    Google App Engine, and modify the application line in `app.yaml`
+    with the application ID registered with Google App Engine.
+
+2.  Install the [App Engine SDK](http://code.google.com/appengine/downloads.html#Google_App_Engine_SDK_for_Python)
+
+3.  Download and unzip the [pre-configured zip file for App Engine](https://github.com/downloads/shopify/shopify_django_app/shopify_appengine-0.1.1.zip).
+
+    Applications for the App Engine need to be self-contained, so
+    the required libraries are included in the zip file along with
+    the projects source code.
+
+    To migrate an existing project to App Engine, just copy the
+    following directories from zip file to your projects root
+    directory:
+
+    * [dateutil](http://pypi.python.org/pypi/python-dateutil)
+    * [django](http://www.allbuttonspressed.com/projects/django-nonrel)
+    * [djangoappengine](http://www.allbuttonspressed.com/projects/djangoappengine)
+    * [djangotoolbox](http://www.allbuttonspressed.com/projects/djangotoolbox)
+    * [pyactiveresource](http://pypi.python.org/pypi/pyactiveresource)
+    * [shopify](http://pypi.python.org/pypi/ShopifyAPI)
+
+    Or follow the links to download the source code for any of the
+    projects, and run `python setup.py build` in the projects root
+    directory, then move `build/lib/*` into the root of this project.
+
+4.  Obtain your applications API Key and Shared Secret, and modify
+    `shopify_settings.py` to use these values.
+
+5.  Start the server
+
+    `python manage.py runserver`
+
+6.  Visit <http://localhost:8000> to view the example.
+
+7.  When you are ready to deploy your application, update the return
+    URL on Shopify to point to your App Engine domain name (e.g.
+    https://APPLICATION-ID.appspot.com/login/finalize).
+
+    Then upload the application:
+
+    `appcfg.py update .`
 
 Questions or problems?
 ----------------------
--- settings.py
+++ settings.py
@@ -1,36 +1,29 @@
 import os
 from shopify_settings import *
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(SITE_ROOT, 'db', 'development.sqlite3'),
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+try:
+    from djangoappengine.settings_base import *
+    USING_APP_ENGINE = True
+except ImportError:
+    USING_APP_ENGINE = False
+
+    DEBUG = True
+    TEMPLATE_DEBUG = DEBUG
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', '
+            'NAME': os.path.join(SITE_ROOT, 'db', 'development.sqlite3'),
+            'USER': '',                      # Not used with sqlite3.
+            'PASSWORD': '',                  # Not used with sqlite3.
+            'HOST': '',                      # Set to empty string for localhost. Not used with 
+            'PORT': '',                      # Set to empty string for default. Not used with sq
+        }
     }
-}
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Toronto'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
-SITE_ID = 1
+    SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
@@ -69,13 +62,15 @@ STATICFILES_DIRS = (
     os.path.join(SITE_ROOT, 'static'),
 )
 
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = ''
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '#w%yp9_5wnupojr=4o0mwap#!)y=q9ovu=o#xnytga7u5^bf27'
@@ -84,7 +79,6 @@ SECRET_KEY = '#w%yp9_5wnupojr=4o0mwap#!)y=q9ovu=o#xnytga7u5^bf27'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
@@ -92,11 +86,14 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
-    'django.core.context_processors.static',
     'django.core.context_processors.request',
     'django.contrib.messages.context_processors.messages',
     'shopify_app.context_processors.current_shop',
 )
+if not USING_APP_ENGINE:
+    TEMPLATE_CONTEXT_PROCESSORS += (
+            'django.core.context_processors.static',
+    )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
@@ -117,12 +114,20 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'home',
     'shopify_app',
 )
+if USING_APP_ENGINE:
+    INSTALLED_APPS += (
+        'djangoappengine',
+        'djangotoolbox',
+    )
+else:
+    INSTALLED_APPS += (
+        'django.contrib.sites',
+        'django.contrib.staticfiles',
+    )
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
