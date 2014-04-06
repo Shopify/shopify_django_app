7b18647c87a71ec6f831f1169f61e2afbfc087c7
JoeReis <josephreis@gmail.com>
2014-04-05 20:05:18 -0600

    fixed url files to be django 1.6 compatible

--- shopify_settings.py
+++ shopify_settings.py
@@ -10,8 +10,10 @@
 # You can ignore this file in git using the following command:
 #   git update-index --assume-unchanged shopify_settings.py
 import os
-SHOPIFY_API_KEY = os.environ.get('SHOPIFY_API_KEY')
-SHOPIFY_API_SECRET = os.environ.get('SHOPIFY_API_SECRET')
+#SHOPIFY_API_KEY = os.environ.get('SHOPIFY_API_KEY')
+#SHOPIFY_API_SECRET = os.environ.get('SHOPIFY_API_SECRET')
+SHOPIFY_API_KEY = '28f7382246434d6925ac9e48bf2d54d7'
+SHOPIFY_API_SECRET = 'e875c3546f11decfd2dde9aaf51ca0ce'
 # See http://api.shopify.com/authentication.html for available scopes
 # to determine the permisssions your app will need.
 SHOPIFY_API_SCOPE = ['read_products', 'read_orders']
