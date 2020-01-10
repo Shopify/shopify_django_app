4e5076d52e0ed72d716bfd4fd65d8faff017f9fc
Tim Anema <tim.anema@shopify.com>
2020-01-10 12:04:46 -0500

    Added back the context_processors

--- shopify_django_app/settings.py
+++ shopify_django_app/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'shopify_app.context_processors.current_shop',
             ],
         },
     },
