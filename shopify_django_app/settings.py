2dd4733a8bfb5e6a08cc949b370df83ef029cd70
Alex Richter <alex.richter@shopify.com>
2020-02-24 11:32:42 -0500

    Move secret key to environment variable

--- shopify_django_app/settings.py
+++ shopify_django_app/settings.py
@@ -19,8 +19,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@*38lzf_hhet!s%5nqokhug)u-d*8h_o+rt=_n(4-k&!pxqdgy'
+# Make this unique and store it as an environment variable. 
+# Do not share it with anyone or commit it to version control.
+SECRET_KEY = os.environ['DJANGO_SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
